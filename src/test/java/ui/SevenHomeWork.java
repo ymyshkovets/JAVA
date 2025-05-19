0524b43d1c50de226a739b8e4796e8ca922a7156
Yuliya Myshkavtes <yuliya.myshkovets@opercredits.com>
2025-05-19 14:13:57 +0300

    test

--- src/test/java/ui/SevenHomeWork.java
+++ src/test/java/ui/SevenHomeWork.java
@@ -1,6 +1,9 @@
 package ui;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 import java.util.Set;
 
 import org.aeonbits.owner.ConfigFactory;
@@ -17,8 +20,10 @@ import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.html5.SessionStorage;
 import org.openqa.selenium.html5.WebStorage;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
@@ -38,7 +43,7 @@ class SevenHomeWork {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        driver = initDriver();
         driver.get(config.getBaseUrl());
         driver.manage().window().maximize();
     }
@@ -47,6 +52,27 @@ class SevenHomeWork {
     void tearDown() {
         driver.getPageSource();
         driver.quit();
+    }
+
+    private WebDriver initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        if (remoteUrl != null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Add headless mode
+            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        return driver;
     }
 
     @Test
